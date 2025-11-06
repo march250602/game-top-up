abe960050c2a04e7af5bcd978c86c7c8f7635c4b
Ratchanon Pawanja <march250602@gmail.com>
2025-11-06 14:20:22 +0700

    update topup api

--- app/api/topup/route.ts
+++ app/api/topup/route.ts
@@ -2,37 +2,33 @@ import { NextResponse } from 'next/server';
 import axios from 'axios';
 
 export async function POST(request: Request) {
-  return NextResponse.json(
-        { success: true },
-      { status: 200 }
-     );
-  // try {
-  //   const { angpaoCode, amount } = await request.json();
+  try {
+    const { angpaoCode, amount } = await request.json();
 
-  //   if (!angpaoCode || !amount) {
-  //     return NextResponse.json(
-  //       { message: 'Missing required fields' },
-  //       { status: 400 }
-  //     );
-  //   }
+    if (!angpaoCode || !amount) {
+      return NextResponse.json(
+        { message: 'Missing required fields' },
+        { status: 400 }
+      );
+    }
 
-  //   // ส่ง request ไปยัง API ภายนอก
-  //   const response = await axios.get(`https://api.xpluem.com/${angpaoCode}/0928388730`);
+    // ส่ง request ไปยัง API ภายนอก
+    const response = await axios.get(`https://api.xpluem.com/${angpaoCode}/0928388730`);
     
-  //   return NextResponse.json(response.data);
-  // } catch (error: any) {
-  //   console.error('API Error:', error);
+    return NextResponse.json(response.data);
+  } catch (error: any) {
+    console.error('API Error:', error);
     
-  //   if (error.response?.data) {
-  //     return NextResponse.json(
-  //       error.response.data,
-  //       { status: error.response.status }
-  //     );
-  //   } else {
-  //     return NextResponse.json(
-  //       { message: 'Internal server error' },
-  //       { status: 500 }
-  //     );
-  //   }
-  // }
+    if (error.response?.data) {
+      return NextResponse.json(
+        error.response.data,
+        { status: error.response.status }
+      );
+    } else {
+      return NextResponse.json(
+        { message: 'Internal server error' },
+        { status: 500 }
+      );
+    }
+  }
 }
